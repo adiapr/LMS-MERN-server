4fe0fd7791a2e3d8a0b38c76176705809f8a76ff
adiapr <da801100@gmail.com>
2022-06-14 15:14:55 +0700

    Express

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-console.log("server.js");
+import express from "express";
+import cors from "cors";
+
+const morgan = require("morgan");
+
+require("dotenv").config();
+
+// create express app
+const app = express();
+
+// apply middleware 
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+// app.use((req, res, next) => {
+//     console.log("This is my own middleware");
+//     next();
+// });
+
+// route 
+app.get("/",    (req, res) => {
+    res.send("you hit server endpoint");
+});
+
+// port 
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
